8ff67678d2264ac93e87636d02aff94769aa1b02
Ray Cheng <ray.cheng.y@gmail.com>
2023-10-16 23:00:48 -0400

    INIT setup using vite.js w/ react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <CharacterEditor />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
