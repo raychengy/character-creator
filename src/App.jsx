f7d30dd27157ed5ed2238d7136107a67a9d439b3
Ray Cheng <ray.cheng.y@gmail.com>
2023-11-02 23:12:07 -0400

    Completed the workshop exercises

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,16 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
+      <div
+        style={{
+          background: " hsl(195deg, 20%, 86%)",
+          width: "100%",
+          height: "40%",
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+        }}
+      ></div>
       <CharacterEditor />
       <Footer />
     </>
